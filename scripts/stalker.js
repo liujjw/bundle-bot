6cc3d3d7a768a3ddbdd37c872b05ff0db62b4185
daisuke <jackyli742@gmail.com>
2021-09-04 18:26:36 -0400

    fix parsing of txpool txs

--- scripts/stalker.js
+++ scripts/stalker.js
@@ -10,7 +10,8 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// ./geth --maxpeers 500 --txpool.globalslots 50000 --txpool-globalqueue 10000
+// might miss some tx since 500 is so small
+// ./geth --maxpeers 500 --txpool.globalslots 500 --txpool-globalqueue 100
 async function main() {
     provider.on('block', async (block) => {
         logger.log("new block appended");
@@ -18,24 +19,29 @@ async function main() {
         // wait not too long for txs
         await sleep(5000);
 
-        let content = await provider.send("txpool_content");
+        // what to do with queued?
+        let content = await provider.send("txpool_content").pending;
+        
         logger.log('fetched tx pool');
         
-        for(let tx of content.slice(content.length - 150)) {
-            if (Object.values(ADDRS.OFFCHAIN_AGG).includes(tx.to)) {
-                fetch(`http://localhost${LOCAL.RUNNER_PORT}/priceUpdate`, {
-                    method: "POST",
-                    headers: "Content-Type: application/json",
-                    body: JSON.stringify(tx)
-                });
-            } else if (tx.to === ADDRS["COMPOUND_COMPTROLLER"]) {
-                fetch(`http://localhost${LOCAL.RUNNER_PORT}/paramUpdate`, {
-                    method: "POST",
-                    headers: "Content-Type: application/json",
-                    body: JSON.stringify(tx)
-                });
+        for(let account in content) {
+            for(let tx in account) {
+                if (Object.values(ADDRS.OFFCHAIN_AGG).includes(tx.to)) {
+                    fetch(`http://localhost${LOCAL.RUNNER_PORT}/priceUpdate`, {
+                        method: "POST",
+                        headers: "Content-Type: application/json",
+                        body: JSON.stringify(tx)
+                    });
+                } else if (tx.to === ADDRS["COMPOUND_COMPTROLLER"]) {
+                    fetch(`http://localhost${LOCAL.RUNNER_PORT}/paramUpdate`, {
+                        method: "POST",
+                        headers: "Content-Type: application/json",
+                        body: JSON.stringify(tx)
+                    });
+                }
             }
         }
+
         logger.log('finished processing tx pool');
     });
 
