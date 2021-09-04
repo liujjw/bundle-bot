22176285059011146e87efad7a790560ba635a01
daisuke <jackyli742@gmail.com>
2021-09-04 19:46:12 -0400

    get working stalker.js

--- scripts/stalker.js
+++ scripts/stalker.js
@@ -4,26 +4,23 @@ const { ADDRS } = require('../lib/Constants');
 const Logger = require('../lib/Logger');
 
 let provider = new ethers.providers.IpcProvider("/home/daisuke/.ethereum/geth.ipc");
+
 let logger = new Logger();
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// might miss some tx since 500 is so small
-// ./geth --maxpeers 500 --txpool.globalslots 500 --txpool-globalqueue 100
+// might miss some tx with small globalslots
+// ./geth --maxpeers 500 
 async function main() {
-    provider.on('block', async (block) => {
-        logger.log("new block appended");
-
-        // wait not too long for txs
-        await sleep(5000);
+    // lags behind by 3-4 seconds sometimes...
+    provider.on('block', async (blockNumber) => {
+        logger.log(`#${blockNumber} appended`);
 
         // what to do with queued?
-        let content = await provider.send("txpool_content").pending;
-        
-        logger.log('fetched tx pool');
-        
+        // instantaneous
+        let content = (await provider.send("txpool_content")).pending;   
         for(let account in content) {
             for(let tx in account) {
                 if (Object.values(ADDRS.OFFCHAIN_AGG).includes(tx.to)) {
@@ -42,10 +39,8 @@ async function main() {
             }
         }
 
-        logger.log('finished processing tx pool');
+        logger.log(`#${blockNumber} finished processing tx pool`);
     });
-
-    while(true) {}    
 }
 
 main().then();
