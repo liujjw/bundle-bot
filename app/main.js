998daaaa5a4602590c89dc8323c2898ea5be13ae
liujjw <liujjw@byakuya.local>
2022-05-23 16:01:47 -0400

    logger and blocknative

--- app/main.js
+++ app/main.js
@@ -5,31 +5,7 @@ const schedule = require("node-schedule");
 const { sleep } = require("../lib/Utils");
 const AccountsDbClient = require("../lib/AccountsDbClient");
 const { PARAMS } = require("../lib/Constants");
-const { createLogger, format, transports } = require("winston");
-const logger = createLogger({
-  level: "info",
-  format: format.combine(
-    format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
-    }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json()
-  ),
-
-  transports: [
-    new transports.File({ filename: "../logs/error.log", level: "error" }),
-    new transports.File({ filename: "../logs/combined.log" }),
-  ],
-});
-
-// if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    })
-  );
-// }
+const logger = require("../lib/Logger");
 
 /**
  * Starts a cluster of services. 
--- app/stalker.js
+++ app/stalker.js
@@ -3,18 +3,60 @@ const Web3 = require("web3");
 const net = require("net");
 const { ADDRS, PARAMS } = require("../lib/Constants");
 const schedule = require("node-schedule");
-
-const web3 = new Web3(
-  new Web3.providers.IpcProvider(process.env.IPC_PROVIDER_ENDPOINT, net)
-);
+const bodyParser = require("body-parser");
+const express = require("express");
 
 // TODO stalk base fee/price drops so that previously unprofitable
 // positions now are profitable
 
 /**
- * @notice DO NOT await fetch calls
+ * @notice DO NOT AWAIT FETCH
+ * @param {*} tx 
  */
-async function main() {
+function send(tx) {
+  if (Object.values(ADDRS.OFFCHAIN_AGG).includes(tx.to)) {
+    fetch(`${process.env.RUNNER_ENDPOINT}/priceUpdate`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(tx),
+    }).catch(e => {
+      process.send(e);
+    });
+  } else if (tx.to === ADDRS["COMPOUND_COMPTROLLER"]) {
+    fetch(`${process.env.RUNNER_ENDPOINT}/paramUpdate`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(tx),
+    }).catch(e => {
+      process.send(e)
+    });
+  }
+}
+
+/**
+ * 
+ */
+function server() {
+  const app = express();
+  app.use(bodyParser.json());
+  app.post("/", async (req, res) => {
+    const tx = req.body;
+    if (tx.status === "pending") {
+      send(tx);
+    }
+    res.send("ok");
+  });
+  app.listen(Number.parseInt(process.env.BLOCKNATIVE_RP_PORT), () => {});
+}
+
+/**
+ * 
+ */
+async function subscribe() {
+  const web3 = new Web3(
+    new Web3.providers.IpcProvider(process.env.IPC_PROVIDER_ENDPOINT, net)
+  );
+
   web3.eth
     .subscribe("pendingTransactions", (error, result) => {
       if (error) {
@@ -24,40 +66,30 @@ async function main() {
     .on("data", async function (txHash) {
       const tx = await web3.eth.getTransaction(txHash);
       if (tx === null) return;
-      
-      if (Object.values(ADDRS.OFFCHAIN_AGG).includes(tx.to)) {
-        fetch(`${process.env.RUNNER_ENDPOINT}/priceUpdate`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(tx),
-        }).catch(e => {
-          process.send(e);
-        });
-      } else if (tx.to === ADDRS["COMPOUND_COMPTROLLER"]) {
-        fetch(`${process.env.RUNNER_ENDPOINT}/paramUpdate`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(tx),
-        }).catch(e => {
-          process.send(e)
-        });
-      }
-    });
-
-    const checkJob = schedule.scheduleJob(PARAMS.CHECK_SCHEDULE, 
-      async function() {
-        fetch(`${process.env.RUNNER_ENDPOINT}/check`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: "",
-        }).catch(e => {
-          process.send(e);
-        });
-      }
-    );
-    checkJob.on("error", (e) => {
-      process.send(`erorr checking with ${e}`);
+      send(tx);
     });
 }
 
-main().then().catch(err => process.send(err));
+/**
+ * 
+ */
+async function check() {
+  const checkJob = schedule.scheduleJob(PARAMS.CHECK_SCHEDULE, 
+    async function() {
+      fetch(`${process.env.RUNNER_ENDPOINT}/check`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: "",
+      }).catch(e => {
+        process.send(e);
+      });
+    }
+  );
+  checkJob.on("error", (e) => {
+    process.send(`erorr checking with ${e}`);
+  });
+}
+
+check().then();
+subscribe().then().catch(err => process.send(err));
+server();
