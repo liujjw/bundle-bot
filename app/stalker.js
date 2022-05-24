4a7103e47e01e210b30da890f8ba4c391207a7a6
liujjw <liujjw@byakuya.local>
2022-05-23 20:53:12 -0400

    chainstack

--- app/stalker.js
+++ app/stalker.js
@@ -5,14 +5,25 @@ const { ADDRS, PARAMS } = require("../lib/Constants");
 const schedule = require("node-schedule");
 const bodyParser = require("body-parser");
 const express = require("express");
+const seenHashes = [];
+const firstSeen = new Map();
 // TODO stalk base fee/price drops so that previously unprofitable
 // positions now are profitable
 
 /**
  * @notice DO NOT AWAIT FETCH
  * @param {*} tx 
+ * @param {*} from
  */
-function send(tx) {  
+function send(tx, from) {  
+  if (seenHashes.includes(tx.hash)) return;
+  seenHashes.push(tx.hash);
+  firstSeen.set(tx.hash, from);
+  if (seenHashes.length > 100) {
+    console.log(firstSeen);
+    seenHashes = [];
+  }
+
   if (Object.values(ADDRS.OFFCHAIN_AGG).includes(tx.to)) {
     fetch(`${process.env.RUNNER_ENDPOINT}/priceUpdate`, {
       method: "POST",
@@ -36,7 +47,7 @@ function send(tx) {
  * Reverse proxy to Runner for ngrok forwarding.
  */
 function server() {
-  // TODO use blocknative as a fallback provider
+  // TODO use blocknative as a fallback provider or complement with a seenHashes array
   // 1) install ngrok node wrapper, start ngrok, take note of forwarding address
   // 2) use forwarding address as blocknative webhook address
   // 3) add code to use as fallback when local node subscribe fails
@@ -56,12 +67,10 @@ function server() {
 
 /**
  * 
+ * @param {*} web3 
+ * @param {*} from 
  */
-async function subscribe() {
-  const web3 = new Web3(
-    new Web3.providers.IpcProvider(process.env.IPC_PROVIDER_ENDPOINT, net)
-  );
-
+async function subscribe(web3, from) {
   web3.eth
     .subscribe("pendingTransactions", (error, result) => {
       if (error) {
@@ -71,7 +80,7 @@ async function subscribe() {
     .on("data", async function (txHash) {
       const tx = await web3.eth.getTransaction(txHash);
       if (tx === null) return;
-      send(tx);
+      send(tx, from);
     });
 }
 
@@ -96,4 +105,16 @@ async function check() {
 }
 
 check().then();
-subscribe().then().catch(err => process.send(err));
+
+const web3IPC = new Web3(
+  new Web3.providers.IpcProvider(process.env.IPC_PROVIDER_ENDPOINT, net)
+);
+subscribe(web3IPC, "ipc").then().catch(err => process.send(err));
+
+const web3Chainstack = new Web3(
+  new Web3.providers.WebsocketProvider(
+    "wss://ws-nd-032-574-945.p2pify.com/704be0327c206b1ccab62eb791083a0a"
+  )
+);
+subscribe(web3Chainstack, "chainstack").then().catch(err => process.send(err));
+
--- terraform-gcp/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-project                  = "liquidate-site"
-credentials_file         = "liquidate-site-5a34f4a5a915.json"
